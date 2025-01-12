f468a1d239e327c4f55c9340c2f6ff093c57e140
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-13 07:02:16 +0800

    pendahuluan

---
+++ 00_pendahuluan.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+Standard Library
+- Go, selain merupakan bahas pemrograman, Go juga menyediakan Standard Library (built-in package) tanpa harus menggunakan package dari luar buatan orang lain
+- Contoh pada kelas Go Dasar, kita sudah belajar package bernama fmt dan errors
+- Selain pacakge tersebut, sebenarnya masih banyak package lain yang bisa kita gunakan
+- Pada materi ini, kita akan coba bahas lebih detail package-package yang terdapat sebagai Standard Library di Go yang sering digunakan sebagai saat kita membuat aplikasi
+- <https://pkg.go.dev/std>
+*/
